66e535fa8023569cc06aed93cd5fae5fc1bb5e22
鸡毛 <shihao.gu@perfma.com>
2019-11-11 21:00:01 +0800

    初始化wpi

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Console } from './console'
 import { Motor } from './motor';
 import { GPIO } from './gpio';
 import { F450 } from './f450';
+import PWM from './libpwm';
 
 
 async function main() {
@@ -9,6 +10,7 @@ async function main() {
   // process.exit(0);
   // const m = new Motor(GPIO.GPIO0);
   // await m.Init();
+  PWM.WiringPiInit();
   const f450 = new F450();
   await f450.Init();
   await f450.GearSetTimeout(1, 5);
