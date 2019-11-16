c43bd8df34d81ab3c93b606609dfb3f5e8e66e4a
鸡毛 <shihao.gu@perfma.com>
2019-11-16 11:04:04 +0800

    提交

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,12 @@ import PWM from './libpwm';
 async function main() {
   PWM.WiringPiInit();
   const f450 = new F450();
-  await f450.Init();
+  // await f450.Init();
   console.log('所有电机初始化完成');
   await f450.GearSetTimeout(1, 5);
   await f450.GearSetTimeout(2, 4);
   await f450.GearSetTimeout(3, 3);
-  await f450.GearSetTimeout(4, 0.7);
-  await f450.GearSetTimeout(3, 3);
+  await f450.GearSetTimeout(4, 0.5);
   f450.GearSet(0);
 }
 
